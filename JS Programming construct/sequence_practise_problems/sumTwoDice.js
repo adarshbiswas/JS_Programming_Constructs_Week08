8754957d3859aa5e34db29c6fea3e47811807b9d
adarsh biswas <officiallyadarsh2003@gmail.com>
2025-03-05 13:05:50 +0530

    [Adarsh Biswas] add: sequence problems submitted

---
+++ JS Programming construct/sequence_practise_problems/sumTwoDice.js
@@ -0,0 +1,6 @@
+
+// Generate two dice rolls and sum them
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+let sum = dice1 + dice2;
+console.log(`Dice 1: ${dice1}, Dice 2: ${dice2}, Sum: ${sum}`);
